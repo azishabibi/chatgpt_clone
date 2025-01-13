82c25cc887504af46d28caf5be6ea5f1f5fcba06
azishabibi <azis2238437609@gmail.com>
2025-01-12 20:08:21 -0800

    reset jwt token reset time

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ load_dotenv()
 # Secret key for JWT
 
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 1440
 
 # Set Hugging Face cache directory to the current directory
 os.environ['HF_HOME'] = os.getcwd()
@@ -90,7 +90,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)  # Default 15 minutes
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)  # Default 15 minutes
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
